c9923b15d8efbdb528b9e6686b82ee6b0359c411
gxtaillon <gxtmm@hotmail.com>
2014-07-18 19:14:34 -0400

    It is now working properly. Compression needs to be improved (pck and uck).

--- Kolmogorov/kc.hs
+++ Kolmogorov/kc.hs
@@ -2,6 +2,12 @@ import Data.Bits
 import Data.List
 import Data.Numbers.Primes
 import qualified Data.Text as T
+import Debug.Trace
+
+-- Because upgrading is a pain in the ***.
+traceShowId :: (Show a) => a -> a
+traceShowId a = trace (show a) a
+
 
 factorLists = map(primeFactors)[0..127]
 factors = nub$concat$factorLists
@@ -10,9 +16,9 @@ encRatio t = fromIntegral (T.length $ encode t) / fromIntegral (T.length t)
 
 -- Behavior is undefined for input characters greater than 0x7f
 encode :: T.Text -> T.Text
-encode t = T.pack $ map (toEnum) $ pck $ rdyFactors
+encode t = T.pack $ map (toEnum) $ pck rdyFactors
     where rawFactors = map (primeFactors.fromEnum) $ T.unpack t
-          idxFactors = map (map(\x->case find(==x)factors of Just i -> i;_->1)) rawFactors
+          idxFactors = map (map(\x->case elemIndex x factors of Just i -> i;_->1)) rawFactors
           rdyFactors = concatMap (\x->[length x]++x) idxFactors
           
           
@@ -32,12 +38,15 @@ ucks (a:r) = if o /= a then ucks ((shift a (-5)):r) ++ [o] else ucks r ++ [o]
 ucks [] = []
 
 
-{-
-decode :: Text -> Text
-decode t = pack $ fs $ sf
-    where pckdFactors = map (fromEnum) $ unpack t
-          rdyFactors = 
-          idxFactors = map ((!!)factors) $ uck pckdFactors
+toIdxFactors :: [Int] -> [[Int]]
+toIdxFactors (a:r) = [fst tup] ++ toIdxFactors (snd tup)
+    where tup = splitAt a r
+toIdxFactors [] = []
+
+decode :: T.Text -> T.Text
+decode t = T.pack $ map (toEnum.product.map((!!)factors)) idxFactors
+    where rdyFactors = uck $ map (fromEnum) $ T.unpack t
+          idxFactors = toIdxFactors rdyFactors
 
 
 
@@ -48,4 +57,4 @@ decode t = pack $ fs $ sf
 
 
 
--}
+
